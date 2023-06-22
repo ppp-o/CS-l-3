6c2ca51ed41487eb08d4891b580ea3b3d9f0752c
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-06-22 17:20:10 +1200

    hello

---
+++ CS-l3.py
@@ -0,0 +1,22 @@
+#Name: Pavneet.kx
+
+from tkinter import *
+# and import messagebox as mb from tkinter
+from tkinter import messagebox as mb
+
+# Create a GUI Window
+root = Tk()
+ 
+# set the size of the GUI Window
+root.geometry("800x450")
+ 
+# set the title of the Window
+root.title("Pavneet's Geo Quiz")
+
+print("hello")
+
+root.mainloop()
+
+
+
+
