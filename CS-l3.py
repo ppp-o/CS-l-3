440e3bf8219635393fdaa530da11887689f2b96b
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-06-22 17:46:15 +1200

    adding background

--- CS-l3.py
+++ CS-l3.py
@@ -4,6 +4,7 @@ from tkinter import *
 # and import messagebox as mb from tkinter
 from tkinter import messagebox as mb
 
+
 # Create a GUI Window
 root = Tk()
  
@@ -12,7 +13,10 @@ root.geometry("800x450")
  
 # set the title of the Window
 root.title("Pavneet's Geo Quiz")
-
+ 
+bg = PhotoImage(file= "homewindow.png")
+Label = Label(root, image = bg)
+Label.place (x = 0, y = 0)
 
 root.mainloop()
 
