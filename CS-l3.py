fa47752c2b0726abd9c8776e89ee4a18d5d23171
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-06-27 09:56:13 +1200

    bg image fits background

--- CS-l3.py
+++ CS-l3.py
@@ -1,4 +1,4 @@
-#Name: Pavneet - testing 
+#Name: Pavneet 
 
 from tkinter import *
 # and import messagebox as mb from tkinter
@@ -9,15 +9,13 @@ from tkinter import messagebox as mb
 root = Tk()
  
 # set the size of the GUI Window
-root.geometry("900x500")
+root.geometry("800x500")
  
 # set the title of the Window
 root.title("Pavneet's Geo Quiz")
 
 # Define Image
-bg = PhotoImage(file= "Homewindow.png")
-#Label = Label(root, image = bg)
-#Label.place (x = 0, y = 0)
+bg = PhotoImage(file="image/Homewindow.new.png")
 
 # Create a Label
 my_label = Label(root, image=bg)
@@ -27,7 +25,13 @@ my_label.place(x=0, y=0, relwidth=1, relheight=1)
 my_frame = Frame(root)
 my_frame.pack(pady=20)
 
+# Add some buttons and now put buttons inside the frame just added
+my_button1 = Button(my_frame, text="click to start")
+my_button1.grid(row=0, column=0, padx=20)
+
+
 root.mainloop()
+
 
 
 
