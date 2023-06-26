72745a5b8edad475a49d39ebeb2212e0966c3ecd
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-06-26 12:00:58 +1200

    making bg image resize with window

--- CS-l3.py
+++ CS-l3.py
@@ -9,14 +9,23 @@ from tkinter import messagebox as mb
 root = Tk()
  
 # set the size of the GUI Window
-root.geometry("800x450")
+root.geometry("900x500")
  
 # set the title of the Window
 root.title("Pavneet's Geo Quiz")
- 
-bg = PhotoImage(file= "homewindow.png")
-Label = Label(root, image = bg)
-Label.place (x = 0, y = 0)
+
+# Define Image
+bg = PhotoImage(file= "Homewindow.png")
+#Label = Label(root, image = bg)
+#Label.place (x = 0, y = 0)
+
+# Create a Label
+my_label = Label(root, image=bg)
+my_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+# Create a frame - for background to stand out
+my_frame = Frame(root)
+my_frame.pack(pady=20)
 
 root.mainloop()
 
