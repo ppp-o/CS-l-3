d5a30238b975c11b562905916944e0889d0770f9
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-07-31 11:33:34 +1200

    new buttons for quix levels

--- quizlevels.py
+++ quizlevels.py
@@ -25,7 +25,7 @@ def prevPage():
 
 Button(
     root, 
-    text="Previous Page", 
+    text="Home", 
     command=prevPage
     ).place(x=228, y=432)
 
@@ -34,5 +34,23 @@ Button(
     text="Next Page", 
     command=nextPage
     ).place(x=128, y=332)
+
+Button(
+    root, 
+    text="easy quiz"
+    "command= have new window that opens"
+).place(x=328, y=532)
+
+Button(
+    root, 
+    text="quiz"
+    "command= have new window that opens"
+).place(x=428, y=632)
+
+Button(
+    root, 
+    text="hard quiz"
+    "command= have new window that opens"
+).place(x=528, y=732)
 
 root.mainloop()
