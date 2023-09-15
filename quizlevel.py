07ffb1947716ea35598b636f468b99932b473a1d
ppp-o <99623992+ppp-o@users.noreply.github.com>
2023-09-15 21:07:11 +1200

    creating login for end users

--- quizlevel.py
+++ quizlevel.py
@@ -1,6 +1,10 @@
 from tkinter import *
+# and import messagebox as mb from tkinter
+from tkinter import messagebox as mb
+import tkinter as tk
 
 root = Tk()
+
 root.title('Pavneets Geo Quiz')
 
 # set the size of the GUI Window
@@ -17,6 +21,33 @@ f = ("Times bold", 14)
 # Define Image
 bg = PhotoImage(file="image/quizlevel.png")
 
+# Create a Label
+my_label = Label(root, image=bg)
+my_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+#labels for end user name and age 
+name_label = tk.Label(root, text="Name:")
+name_label.pack()
+
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+name_error_label = tk.Label(root, text="", fg="red")
+name_error_label.pack()
+
+age_label = tk.Label(root, text="Age:")
+age_label.pack()
+
+age_entry = tk.Entry(root)
+age_entry.pack()
+
+age_error_label = tk.Label(root, text="", fg="red")
+age_error_label.pack()
+
+startquiz_button = tk.Button(root, text="Start quiz")
+startquiz_button.pack()
+
+
 def nextPage():
     root.destroy()
     import quiz
@@ -25,17 +56,23 @@ def prevPage():
     root.destroy()
     import home
 
-
 Button(
     root, 
     text="Home", 
     command=prevPage
     ).place(x=150, y=400)
 
-Button(
+"""Button(
     root, 
     text="Start quiz", 
     command=nextPage
-    ).place(x=550, y=400)
+    ).place(x=550, y=400)"""
+
+def Close():
+    root.destroy()
+  
+# Button for quiting quiz
+exit_button = Button(root, text="Quit", command=Close)
+exit_button.place(x=550, y=400)
 
 root.mainloop()
